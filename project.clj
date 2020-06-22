e51dad6def753061b1abe0d1b9a93f9e816b90d1
Matthew Boston <matthew@matthewboston.com>
2020-06-22 10:34:28 -0600

    update ring-core 1.8.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/core.incubator "0.1.4"]
-                 [ring/ring-core "1.8.0" :scope "provided" :exclusions [commons-codec]]
+                 [ring/ring-core "1.8.1" :scope "provided" :exclusions [commons-codec]]
                  [ring-mock "0.1.5" :scope "test"]
                  [compojure "1.6.1" :exclusions [commons-codec joda-time]]
                  [org.clojure/data.codec "0.1.1"]
