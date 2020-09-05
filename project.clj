defa3d15a9af529a59cb5bbef65849be98c7ab22
Matthew Boston <matthew@matthewboston.com>
2020-09-05 09:41:34 -0600

    0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bostonaholic/ring-okta "0.4.0"
+(defproject bostonaholic/ring-okta "0.4.1-SNAPSHOT"
   :description "Ring middleware for Okta Single Sign-on"
   :url "https://github.com/bostonaholic/ring-okta"
   :license {:name "Eclipse Public License"
