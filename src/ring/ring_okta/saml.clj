d9120c929da888f62736ae74d3bb6790fe5a7e6b
Matthew Boston <matthew@matthewboston.com>
2020-09-07 22:06:09 -0600

    refactor: make function more referentially transparent

--- src/ring/ring_okta/saml.clj
+++ src/ring/ring_okta/saml.clj
@@ -3,9 +3,8 @@
             [clojure.string :as string])
   (:import (com.okta.saml SAMLValidator)))
 
-(defn- get-valid-user-id [saml-response okta-config]
-  (let [validator (SAMLValidator.)
-        config (.getConfiguration validator okta-config)
+(defn- get-valid-user-id [saml-response okta-config validator]
+  (let [config (.getConfiguration validator okta-config)
         decoded-saml-response (String. (b64/decode (.getBytes saml-response "UTF-8")))
         valid-saml-response (.getSAMLResponse validator decoded-saml-response config)]
     (.getUserID valid-saml-response)))
@@ -13,4 +12,4 @@
 (defn respond-to-okta-post [okta-config params]
   {:redirect-url (:RelayState params)
    :authenticated-user-email (string/lower-case
-                              (get-valid-user-id (:SAMLResponse params) okta-config))})
+                              (get-valid-user-id (:SAMLResponse params) okta-config (SAMLValidator.)))})
