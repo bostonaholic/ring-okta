6d45b7c25a8472f9da6d484d8c5043bb527b65a8
Matthew Boston <matthew@matthewboston.com>
2020-09-12 22:58:19 -0600

    add binding for readability

--- src/ring/ring_okta/session.clj
+++ src/ring/ring_okta/session.clj
@@ -5,7 +5,8 @@
             [ring.util.response :as response]))
 
 (defn login [{:keys [okta-config-location params]}]
-  (let [okta-response (saml/respond-to-okta-post (with-open [r (io/reader okta-config-location)] (slurp r)) params)]
+  (let [okta-config-contents (with-open [r (io/reader okta-config-location)] (slurp r))
+        okta-response (saml/respond-to-okta-post okta-config-contents params)]
     (assoc-in
      (response/redirect-after-post (:redirect-url okta-response))
      [:session :okta/user]
