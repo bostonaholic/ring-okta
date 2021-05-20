2e69639cfcfe045b3805c677343b611642895451
Matthew Boston <matthew@matthewboston.com>
2021-05-20 09:25:54 -0600

    upgrade ring-core 1.9.3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
                  [org.clojure/core.incubator "0.1.4"]
-                 [ring/ring-core "1.9.2" :scope "provided" :exclusions [commons-codec]]
+                 [ring/ring-core "1.9.3" :scope "provided" :exclusions [commons-codec]]
                  [ring-mock "0.1.5" :scope "test"]
                  [compojure "1.6.2" :exclusions [org.clojure/clojure commons-codec joda-time]]
                  [org.clojure/data.codec "0.1.1"]
