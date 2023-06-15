bccd84579e1cfe65e835a07ca0c8c0be19d50c19
Matthew Boston <matthew@matthewboston.com>
2023-06-15 10:01:42 -0600

    1.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bostonaholic/ring-okta "1.0.6"
+(defproject bostonaholic/ring-okta "1.0.7-SNAPSHOT"
   :description "Ring middleware for Okta Single Sign-on"
   :url "https://github.com/bostonaholic/ring-okta"
   :license {:name "The MIT License (MIT)"
